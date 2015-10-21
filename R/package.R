bf59529af96bb5576e70d98539940e90a3b0d487
Thibaut Jombart <thibautjombart@gmail.com>
2015-10-21 17:38:35 +0100

    adding right version of the R files, with Roxyg2

---
+++ R/package.R
@@ -0,0 +1,23 @@
+#'
+#' outbreaker2: disease outbreak reconstruction using epidemiological and genetic data
+#'
+#' This package re-implements the model introduced by Jombart et al. (PLoS Comput. Biol, 2014) for disease outbreak reconstruction using epidemiological and genetic data. This new version is an alternative to the original package 'outbreaker', relying on a more parsimonious use of C code and implementing new, more flexible sampling approaches.
+#'
+#'
+#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
+#'
+#' @name outbreaker.package
+#'
+#' @import parallel
+#'
+#' @importFrom utils packageDescription
+#'
+#' @importFrom graphics plot barplot lines mtext matplot legend
+#'
+#' @import igraph
+#'
+#' @importFrom adegenet num2col transp funky findMutations
+#'
+#' @importFrom ape dist.dna as.DNAbin
+#'
+NULL

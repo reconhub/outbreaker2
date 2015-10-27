7fbaad0ff04cf0bdf17f1921c9f921f5614726b2
Thibaut <thibautjombart@gmail.com>
2015-10-27 20:36:01 +0000

    adding move files - forgot it

---
+++ R/moves.R
@@ -0,0 +1,45 @@
+#' Movements of augmented data and parameters for outbreaker2
+#'
+#' These functions are used to move in the parameter space in the MCMC.
+#'
+#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
+#'
+#' @rdname moves
+#'
+#' @param D a matrix of pairwise genetic distances
+#' @param gen.length length of the genetic sequences
+#' @param ances a vector of indices of ancestors
+#' @param mu a mutation rate
+#' @param times a vector of integers indicating dates of infections of the cases
+#' @param log.w a vector of log probabilities of time intervals (between infections), starting at p(T=0)
+#' @param dev a deviation from the previous mu
+#' @param lunif a logged random variate from Unif(0,1)
+#' @importFrom stats rnorm
+#'
+move.mu <- function(D, gen.length, ances, mu, dev, lunif){ # assumes symmetric proposal
+    new.mu <- mu + dev
+
+    ## escape if new.mu<0
+    if(new.mu<0) return(mu)
+
+    ## compute log ratio
+    logratio <- post.genetic(D=D, gen.length=gen.length, ances=ances, mu=new.mu) -
+        post.genetic(D=D, gen.length=gen.length, ances=ances, mu=mu)
+
+    ## accept/reject
+    if(logratio >= lunif) return(new.mu)
+    return(mu)
+} # end post.genetic
+
+
+
+
+
+## #' @rdname posteriors
+## #' @export
+## #'
+## move.all <- function(times, D, gen.length, log.w, ances, mu){
+##     return(ll.timing.infections(times=times, ances=ances, log.w=log.w) +
+##            ll.genetic(D=D, ances=ances, mu=mu, gen.length=gen.length) +
+##            prior.mu(mu))
+## }

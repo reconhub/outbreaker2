daf5b6c9127300975f4f2bd28bde67bbd6c44139
Thibaut Jombart <thibautjombart@gmail.com>
2015-10-30 18:57:17 +0000

    adding test to achieve 100% coverage

--- tests/testthat/test_config.R
+++ tests/testthat/test_config.R
@@ -9,11 +9,13 @@ test_that("test: data are processed fine", {
     ## get data
     x <- fakeOutbreak
     out <- outbreaker.data(dates=x$collecDates, dna=x$dat$dna, w.dens=x$w)
+    out.nodna <- outbreaker.data(dates=x$collecDates, w.dens=x$w)
 
     ## check output
     expect_is(out, "list")
     expect_is(out$D, "matrix")
     expect_equal(out$MAX.RANGE, 11)
+    expect_equal(out.nodna$L, 0)
     expect_error(outbreaker.data())
     expect_error(outbreaker.data(dates=1, w.dens=c(0,-1)))
     expect_error(outbreaker.data(dates=1, w.dens=c(0,1), f.dens=c(0,-1)))

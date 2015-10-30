a9fe728326aea574320799b0aff784a2f9875e73
Thibaut Jombart <thibautjombart@gmail.com>
2015-10-30 19:13:43 +0000

    adding more tests

--- tests/testthat/test_config.R
+++ tests/testthat/test_config.R
@@ -30,16 +30,21 @@ test_that("test: settings are processed fine", {
     ## get data
     x <- fakeOutbreak
     dat <- outbreaker.data(dates=x$collecDates, dna=x$dat$dna, w.dens=x$w)
+    dat.nodna <- dat
+    dat.nodna$dna <- NULL
 
     ## check output
     expect_is(outbreaker.config(), "list")
     expect_is(outbreaker.config(data=dat), "list")
+    expect_equal(outbreaker.config(init.tree="star", data=dat)$ances, c(NA, rep(1,29)))
     expect_error(outbreaker.config(uknownarg=123))
     expect_error(outbreaker.config(init.tree="wrongtreeinit"))
     expect_error(outbreaker.config(init.mu=-5))
     expect_error(outbreaker.config(n.iter=0))
     expect_error(outbreaker.config(sample.every=0))
-    expect_equal(outbreaker.config(init.tree="star", data=dat)$ances, c(NA, rep(1,29)))
+    expect_error(outbreaker.config(init.tree=1:5, data=dat))
+    expect_warning(outbreaker.config(init.tree="seqTrack", data=dat.nodna))
+    expect_warning(outbreaker.config(init.tree=rep(-1,dat$N), data=dat))
 })
 
 

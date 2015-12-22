f5a1c9b823ff009296f4a0c5f87a08463dd9e2c1
Thibaut <thibautjombart@gmail.com>
2015-12-22 13:55:11 +0000

    attempt at closure programming for runif

--- R/outbreaker.rand.vec.R
+++ R/outbreaker.rand.vec.R
@@ -24,3 +24,50 @@ outbreaker.rand.vec <- function(config)
     ## RETURN ##
     return(out)
 } # end outbreaker.rand.vec
+
+
+
+counter <- function(batch.size=10){
+    i <- 0
+    out <- function(){
+        if(i<batch.size){
+            i <<- i+1
+        } else {
+            i <<- 0
+        }
+        return(i)
+    }
+    return(out)
+}
+
+
+outbreaker.log.runif <- function(size.batch=10){
+    ## initialize array
+    values <- log(runif(size.batch))
+
+    ## initialize counter
+    counter <- 0
+
+    ## create returned function
+    out <- function(n){
+        ## read from array if enough values
+        if((counter+n) <= size.batch){
+            cat("\n reading values from vector\n")
+            on.exit(return(values[seq.int(counter+1, counter+n)]))
+            counter <<- counter+n
+        } else {
+            ## regenerate vector of values
+            cat("\n generating vector\n")
+            if(n>size.batch) {
+                size.batch <<- n
+            }
+            values <<- log(runif(size.batch))
+            counter <<- 0
+
+            return(out(n))
+        }
+    }
+
+    ## return output function
+    return(out)
+}

d7a8de5dd4ebc409cef60fa20fd10357e12290c7
Thibaut <thibautjombart@gmail.com>
2015-12-25 15:02:03 +0000

    fixed broken expl

--- R/outbreaker.R
+++ R/outbreaker.R
@@ -13,26 +13,9 @@
 #' N (2014).  Bayesian reconstruction of disease outbreaks by combining
 #' epidemiologic and genomic data. PLoS Computational Biology.
 #'
-#' @seealso \code{outbreaker.data} to process input data, and \code{outbreaker.config} to process/set up parameters
+#' @seealso \code{\link{outbreaker.data}} to process input data, and \code{\link{outbreaker.config}} to process/set up parameters
 #'
-#' @param dna the DNA sequences in \code{DNAbin} format (see
-#' \code{\link[ape]{read.dna}} in the ape package); this can be imported from a
-#' fasta file (extension .fa, .fas, or .fasta) using \code{adegenet}'s function
-#' \link[adegenet]{fasta2DNAbin}.
-#'
-#' @param dates a vector indicating the collection dates, provided either as
-#' integer numbers or in a usual date format such as \code{Date} or
-#' \code{POSIXct} format. By convention, zero will indicate the oldest date.
-#'
-#' @param w.dens a vector of numeric values indicating the generation time
-#' distribution, reflecting the infectious potential of a case t=0, 1, 2, ...
-#' time steps after infection. By convention, w.dens[1]=0, meaning that an
-#' newly infected patient cannot be instantaneously infectious. If not
-#' standardized, this distribution is rescaled to sum to 1.
-#'
-#' @param f.dens similar to \code{w.dens}, except that this is the distribution
-#' of the colonization time, i.e. time interval during which the pathogen can
-#' be sampled from the patient.
+#' @param data a list of named items containing input data as returned by \code{\link{outbreaker.data}}
 #'
 #' @param config a set of settings as returned by \code{\link{outbreaker.config}}
 #'
@@ -49,7 +32,8 @@
 #' w <- fakeOutbreak$w
 #'
 #' ## run outbreaker
-#' out <- outbreaker(dna=dat$dna, dates=dat$onset, w.dens=w, config=list(n.iter=100, sample.every=10))
+#' out <- outbreaker(data=list(dna=dat$dna, dates=dat$onset, w.dens=w),
+#' config=list(n.iter=100, sample.every=10))
 #'
 #'
 outbreaker <- function(data=outbreaker.data(),

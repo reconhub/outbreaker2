617c828d0eb1eec52e36cc7987077ed7ce8ee712
Thibaut Jombart <thibautjombart@gmail.com>
2016-03-09 01:22:39 +0000

    adding tests on auxil movement functions

--- tests/testthat/test_non_exported.R
+++ tests/testthat/test_non_exported.R
@@ -25,13 +25,27 @@ test_that("test: find.possible.ances", {
 test_that("Auxiliary functions for ancestries are working", {
     ## skip on CRAN
     skip_on_cran()
+    rm(list=ls())
 
     ## generate data
     ances <- c(NA,1,1,2,2)
     t.inf <- c(0, 2,3, 5,7)
-    param <- outbreaker.mcmc.init(
-        config=outbreaker.config(init.tree=ances, init.t.inf=t.inf),
-        data=outbreaker.data())
+    data <- outbreaker.data(dates=t.inf+1)
+    config <- outbreaker.config(init.tree=ances, init.t.inf=t.inf)
+    config2 <- config
+    config2$move.ances <- c(rep(TRUE,4),FALSE)
+    param <- outbreaker.mcmc.init(config=config, data=data)
+
+    ## test can be ances
+    expect_equal(can.move.ances(param, config), c(FALSE,rep(TRUE,4)))
+    expect_equal(can.move.ances(param, config2), c(FALSE, TRUE, TRUE, TRUE, FALSE))
+
+    ## test ancestor selection
+    set.seed(1)
+    to.move <- replicate(10,select.ances.to.move(param,config))
+    expect_equal(to.move, c(3,3,4,5,2,5,5,4,4,2))
+    to.move2 <- replicate(10,select.ances.to.move(param,config2))
+    expect_equal(to.move2, c(2,2,4,3,4,3,4,4,3,4))
 
     ## tests non swapping (imported case)
     expect_warning(res <- outbreaker2:::swap.ances(param, 1))

156f5dc680751e3511e318c24e86fc3de811c290
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-07 14:18:05 +0100

    fixed typo in function call

--- R/outbreaker.rand.vec.R
+++ R/outbreaker.rand.vec.R
@@ -17,7 +17,7 @@ outbreaker.rand.vec <- function(config)
 
     ## CREATE FAST RANDOM NUMBER GENERATORS ##
     out$log.runif1 <- make.fast.rand1(f=runif, batch.size=config$batch.size, log=TRUE)
-    out$mu.rnorm1 <- make.fast.rand1(mean=0, sd=config$sd.mu, f=rnorm, batch.size=config$batch.size, log=FALSE)
+    out$rnorm1 <- make.fast.rand1(mean=0, sd=config$sd.mu, f=rnorm, batch.size=config$batch.size, log=FALSE)
 
     ## RETURN ##
     return(out)

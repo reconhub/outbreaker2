83e8c6b033d9177bcf51b8b7e6c73182106a50a8
thibautjombart <thibautjombart@gmail.com>
2016-04-19 18:21:08 +0100

    this function now returns a list of loglike functions with enclosed data

--- R/outbreaker.create.loglike.R
+++ R/outbreaker.create.loglike.R
@@ -1,73 +1,48 @@
-#' Create likelihood functions for outbreaker
-#'
-#' This function creates a named list of functions, each of which are compute log-likelihood components of outbreaker.
-#' If these functions are provided as input, they will be used. Otherwise, default functions will be used.
-#'
-#' User-provided functions will be checked to ensure the right arguments are present. All log-likelihood functions should have to following arguments:
-#' \describe{
-#' \item{data}{a list of named items containing input data as returned by \code{\link{outbreaker.data}}}
-#' \item{param}{a list of parameters as returned by \code{outbreaker.mcmc.init}}
-#' }
-#'
-#' Note that unlike \code{outbreaker.create.moves}, likelihood functions have no mandatory names. They only need to be compatible with the movement functions used in \code{outbreaker.create.moves}. See 'details' for defaults.
-#'
-#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
-#'
-#' @param ... a named list (see details) of functions, each computing a log-likelihood component.
-#' @param loglike a list of functions as returned by \code{outbreaker.create.loglike}
-#'
-#' @details
-#' The likelihood functions used by default are:
-#' \describe{
-#' \item{timing.infections}{corresponds to the probability of delays between cases (generation time)}
-#' \item{timing.sampling}{corresponds to the probability of delays between infection and reporting}
-#' \item{timing}{corresponds to the probability of all delays}
-#' \item{genetic}{corresponds to the probability of the genetic sequences given the transmission tree}
-#' \item{all}{corresponds to the overal probability of the data}
-#' }
-#'
-#'
-#' @return a list of named functions
-#'
-outbreaker.create.loglike <- function(..., loglike=NULL){
-    ## PROCESS ... ONLY IF NO MOVES IS PASSED
-    if(is.null(loglike)){
-        loglike <- list(...)
-    }
+## #' Create likelihood functions for outbreaker
+## #'
+## #' This function creates a named list of functions, each of which are compute log-likelihood components of outbreaker.
+## #' If these functions are provided as input, they will be used. Otherwise, default functions will be used.
+## #'
+## #' User-provided functions will be checked to ensure the right arguments are present. All log-likelihood functions should have to following arguments:
+## #' \describe{
+## #' \item{data}{a list of named items containing input data as returned by \code{\link{outbreaker.data}}}
+## #' \item{param}{a list of parameters as returned by \code{outbreaker.mcmc.init}}
+## #' }
+## #'
+## #' Note that unlike \code{outbreaker.create.moves}, likelihood functions have no mandatory names. They only need to be compatible with the movement functions used in \code{outbreaker.create.moves}. See 'details' for defaults.
+## #'
+## #' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
+## #'
+## #' @param ... a named list (see details) of functions, each computing a log-likelihood component.
+## #' @param loglike a list of functions as returned by \code{outbreaker.create.loglike}
+## #'
+## #' @details
+## #' The likelihood functions used by default are:
+## #' \describe{
+## #' \item{timing.infections}{corresponds to the probability of delays between cases (generation time)}
+## #' \item{timing.sampling}{corresponds to the probability of delays between infection and reporting}
+## #' \item{timing}{corresponds to the probability of all delays}
+## #' \item{genetic}{corresponds to the probability of the genetic sequences given the transmission tree}
+## #' \item{all}{corresponds to the overal probability of the data}
+## #' }
+## #'
+## #'
+## #' @return a list of named functions
+## #'
 
+##
+## This function creates a named list of log-likelihood functions with enclosed data
+##
+outbreaker.create.loglike <- function(data){
 
-    ## SET DEFAULTS ##
-    defaults <- list(ll.timing.infections = ll.timing.infections,
-                     ll.timing.sampling = ll.timing.sampling,
-                     ll.timing = ll.timing,
-                     ll.genetic = ll.genetic,
-                     ll.reporting = ll.reporting,
-                     ll.all = ll.all
-                     )
+    ## These are all the functions generating various log-likelihood functions; they are assumed to
+    ## follow alphabetic order
 
-    ## MODIFY DEFAULTS WITH ARGUMENTS ##
-    loglike <- modify.defaults(defaults, loglike, strict=FALSE)
+    default.functions <- list(genetic = make.ll.genetic,
+                              reporting = make.ll.reporting,
+                              timing.infections = make.ll.timing.infections,
+                              timing.sampling = make.ll.timing.sampling
+                              )
+    out <- lapply(default.functions, function(f) f(data))
 
-
-    ## CHECK FUNCTIONS ##
-    check.function.args <- function(f){
-        args <- names(formals(f))
-        if(identical(sort(args), c("data", "param")) ||
-           identical(sort(args), c("data", "i", "param"))) {
-            return(TRUE)
-        } else {
-            return(FALSE)
-        }
-    }
-
-    args.checks <- sapply(unlist(loglike), check.function.args)
-    if(!all(args.checks)){
-        culprits <- names(args.checks)[!args.checks]
-        culprits <- paste(culprits, collapse=",")
-        stop("problems in movements of: ", culprits, "\narguments shoud be: 'data', 'param' (and optionally 'i' for local likelihoods)")
-    }
-
-    ## RETURN ##
-    return(loglike)
-
-} # end outbreaker.create.loglike
+}

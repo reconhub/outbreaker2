f37faf5fb93f9580506bf8a1ecf51b5b2fa5cae0
thibautjombart <thibautjombart@gmail.com>
2016-04-19 18:10:36 +0100

    removed functions summing various likelihoods; likelihood functions are no longer exported; they have been replaced by make.ll.xxx which enclose data with the various likelihood functions; tests will fail for now

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -1,106 +1,107 @@
-#' Internal functions for outbreaker2
-#'
-#' These functions compute various likelihoods used in outbreaker.
-#'
-#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
-#'
-#' @name likelihoods
-#' @export
-#'
-#' @param data a list of named items containing input data as returned by \code{\link{outbreaker.data}}
-#' @param param a list containing parameters as returned by \code{outbreaker.mcmc.init}
-#' @param i an optional vector of integers, indicating subset of cases included in the likelihood computation; if NULL (default), all cases are used.
-ll.timing.infections <- function(data, param, i=NULL){
-    ## compute delays between infection dates of cases and of their ancestors
-    T <- param$current.t.inf[i] - param$current.t.inf[param$current.alpha[i]]
-
-    ## avoid over-shooting: delays outside the range of columns in pre-computed log-densities
-    ## (data$log.w.dens) will give a likelihood of zero
-    if(any(T<1 | T>ncol(data$log.w.dens), na.rm=TRUE)) return(-Inf)
-
-    ## return
-    sum(data$log.w.dens[cbind(param$current.kappa[i], T)], na.rm=TRUE)
-}
 
 
-
-
-
-#' @rdname likelihoods
-#' @export
-#'
-ll.timing.sampling <- function(data, param, i=NULL){
-    ## compute delays
-    T <- data$dates[i] - param$current.t.inf[i]
-    T <- T[!is.na(T)]
-
-    ## avoid over-shooting
-    if(any(T<1 | T>length(data$log.f.dens))) return(-Inf)
-
-    ## return
-    sum(data$log.f.dens[T], na.rm=TRUE)
-}
-
-
-
-
-
-#' @rdname likelihoods
-#' @export
-#'
-ll.timing <- function(data, param, i=NULL){
-    ll.timing.infections(data=data, param=param, i=i) +
-           ll.timing.sampling(data=data, param=param, i=i)
-}
-
-
-## NOTE: if the data can be bound to the ll functions via closures, we
-## can get about a 1 us (50%) speed up on the calls.  The same is not
-## really possible for param of course.  Moving from $... to [["..."]]
-## looks to save a small amount too.
+## In all of these functions:
+## --------------------------
 ##
-## You _might_ get slightly better performance, especially as the
-## number of things grows, by factoring as
-##  log(mu / (1 - mu)) * sum(nmut) + length(nmut) * log(1 - mu) * L
-## which limits to 2 operations rather than 2*n
+## we return a function in which 'data' is enclosed this is substantially faster than having to
+## pass the extra 'data' argument all the time, which does not change anyway; only things which
+## effectively change are the parameters and augmented data, all stored in 'param', and the
+## indices of cases for which we compute the likelihood 'i'.
 
-#' @rdname likelihoods
-#' @export
-#'
-ll.genetic <- function(data, param, i=NULL){
-    ## discard cases with no ancestors to avoid subsetting data$D with 'NA'
-    i <- i[!is.na(param$current.alpha[i])]
+## Arguments are:
+##
+## 'data' a list of named items containing input data as returned by \code{\link{outbreaker.data}}
+## 'param' a list containing parameters as returned by \code{outbreaker.mcmc.init}
+## 'i' an optional vector of integers, indicating subset of cases included in the likelihood computation; if NULL (default), all cases are used.
 
-    ## likelihood is based on the number of mutations between a case and its ancestor;
-    ## these are extracted from a pairwise genetic distance matrix (data$D)
-    nmut <- data$D[cbind(i, param$current.alpha[i], deparse.level=0)]
-    sum(log(param$current.mu)*nmut + log(1 - param$current.mu)*
-               (data$L - nmut), na.rm=TRUE)
+
+
+
+## This likelihood corresponds to the probability of observing infection dates of cases given the
+## infection dates of their ancestors.
+
+make.ll.timing.infections <- function(data){
+
+     function(param, i=NULL){
+
+        ## compute delays between infection dates of cases and of their ancestors
+        T <- param$current.t.inf[i] - param$current.t.inf[param$current.alpha[i]]
+
+        ## avoid over-shooting: delays outside the range of columns in pre-computed log-densities
+        ## (data$log.w.dens) will give a likelihood of zero
+        if(any(T<1 | T>ncol(data$log.w.dens), na.rm=TRUE)) return(-Inf)
+
+        ## return sum of log-densities
+        sum(data$log.w.dens[cbind(param$current.kappa[i], T)], na.rm=TRUE)
+    }
+}
+
+
+
+
+## This likelihood corresponds to the probability of reporting dates of cases given their
+## infection dates.
+
+make.ll.timing.sampling <- function(data){
+
+    function(param, i=NULL){
+
+        ## compute delays
+        T <- data$dates[i] - param$current.t.inf[i]
+        T <- T[!is.na(T)]
+
+        ## avoid over-shooting
+        if(any(T<1 | T>length(data$log.f.dens))) return(-Inf)
+
+        ## return
+        sum(data$log.f.dens[T], na.rm=TRUE)
+    }
+}
+
+
+
+
+## This likelihood corresponds to the probability of observing a number of mutations between cases
+## and their ancestors.
+
+make.ll.genetic <- function(data){
+
+    function(param, i=NULL){
+
+        ## discard cases with no ancestors to avoid subsetting data$D with 'NA'
+        i <- i[!is.na(param$current.alpha[i])]
+
+        ## likelihood is based on the number of mutations between a case and its ancestor;
+        ## these are extracted from a pairwise genetic distance matrix (data$D)
+        nmut <- data$D[cbind(i, param$current.alpha[i], deparse.level=0)]
+
+        ## the log-likelihood is computed as: sum(mu^nmut + (1-mu)^(L-nmut))
+        ## with:
+        ## 'mu' is the mutation probability
+        ## 'L' the number of sites in the alignment
+        ## 'nmut' the number of mutations between an ancestor and its descendent
+        ##
+        ## for computer efficiency, we re-factorise it as:
+        ##  log(mu / (1 - mu)) * sum(nmut) + length(nmut) * log(1 - mu) * L
+        ## which limits to 2 operations rather than 2*n
+        ## (tip from Rich Fitzjohn)
+        log(param$current.mu / (1 - param$current.mu)) * sum(nmut) + length(nmut) * log(1 - param$current.mu) * data$L
+    }
+
 }
 
 
 
 
 
-#' @rdname likelihoods
-#' @export
-#' @importFrom stats dgeom
-ll.reporting <- function(data, param, i=NULL){
-    sum(dgeom(param$current.kappa[i]-1, prob=param$current.pi, log=TRUE),na.rm=TRUE)
+## This likelihood corresponds to the probability of a given number of unreported cases on an ancestry.
+
+make.ll.reporting <- function(data){
+
+    ## the likelihood is given by a geometric distribution with probability 'pi' to report a case
+    function(param, i=NULL){
+        sum(stats::dgeom(param$current.kappa[i]-1,
+                  prob=param$current.pi,
+                  log=TRUE), na.rm=TRUE)
+    }
 }
-
-
-
-
-
-#' @rdname likelihoods
-#' @export
-#'
-ll.all <- function(data, param, i=NULL){
-    ## NOTE: this should probably be replaced by a function iterating over all likelihood functions
-    ll.timing(data=data, param=param, i=i) +
-        ll.genetic(data=data, param=param, i=i) +
-            ll.reporting(data=data, param=param, i=i)
-
-}
-

b511c13b0490521a6d03820dcd50d8b9e4326c5f
thibautjombart <thibautjombart@gmail.com>
2016-04-19 10:50:59 +0100

    removing returns

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -11,15 +11,16 @@
 #' @param param a list containing parameters as returned by \code{outbreaker.mcmc.init}
 #' @param i an optional vector of integers, indicating subset of cases included in the likelihood computation; if NULL (default), all cases are used.
 ll.timing.infections <- function(data, param, i=NULL){
-    ## compute delays
+    ## compute delays between infection dates of cases and of their ancestors
     T <- param$current.t.inf[i] - param$current.t.inf[param$current.alpha[i]]
 
-    ## avoid over-shooting
+    ## avoid over-shooting: delays outside the range of columns in pre-computed log-densities
+    ## (data$log.w.dens) will give a likelihood of zero
     if(any(T<1 | T>ncol(data$log.w.dens), na.rm=TRUE)) return(-Inf)
 
     ## return
-    return(sum(data$log.w.dens[cbind(param$current.kappa[i], T)], na.rm=TRUE))
-} # end ll.timing.infections
+    sum(data$log.w.dens[cbind(param$current.kappa[i], T)], na.rm=TRUE)
+}
 
 
 
@@ -37,8 +38,8 @@ ll.timing.sampling <- function(data, param, i=NULL){
     if(any(T<1 | T>length(data$log.f.dens))) return(-Inf)
 
     ## return
-    return(sum(data$log.f.dens[T], na.rm=TRUE))
-} # end ll.timing.sampling
+    sum(data$log.f.dens[T], na.rm=TRUE)
+}
 
 
 
@@ -48,10 +49,9 @@ ll.timing.sampling <- function(data, param, i=NULL){
 #' @export
 #'
 ll.timing <- function(data, param, i=NULL){
-    ## compute likelihood
-    return(ll.timing.infections(data=data, param=param, i=i) +
-           ll.timing.sampling(data=data, param=param, i=i))
-} # end ll.timing
+    ll.timing.infections(data=data, param=param, i=i) +
+           ll.timing.sampling(data=data, param=param, i=i)
+}
 
 
 ## NOTE: if the data can be bound to the ll functions via closures, we
@@ -74,8 +74,8 @@ ll.genetic <- function(data, param, i=NULL){
     ## likelihood is based on the number of mutations between a case and its ancestor;
     ## these are extracted from a pairwise genetic distance matrix (data$D)
     nmut <- data$D[cbind(i, param$current.alpha[i], deparse.level=0)]
-    return(sum(log(param$current.mu)*nmut + log(1 - param$current.mu)*
-               (data$L - nmut), na.rm=TRUE))
+    sum(log(param$current.mu)*nmut + log(1 - param$current.mu)*
+               (data$L - nmut), na.rm=TRUE)
 } # end ll.genetic
 
 
@@ -86,7 +86,7 @@ ll.genetic <- function(data, param, i=NULL){
 #' @export
 #' @importFrom stats dgeom
 ll.reporting <- function(data, param, i=NULL){
-    return(sum(dgeom(param$current.kappa[i]-1, prob=param$current.pi, log=TRUE),na.rm=TRUE))
+    sum(dgeom(param$current.kappa[i]-1, prob=param$current.pi, log=TRUE),na.rm=TRUE)
 } # end ll.reporting
 
 
@@ -98,9 +98,9 @@ ll.reporting <- function(data, param, i=NULL){
 #'
 ll.all <- function(data, param, i=NULL){
     ## NOTE: this should probably be replaced by a function iterating over all likelihood functions
-    return(ll.timing(data=data, param=param, i=i) +
-           ll.genetic(data=data, param=param, i=i) +
-           ll.reporting(data=data, param=param, i=i)
-           )
+    ll.timing(data=data, param=param, i=i) +
+        ll.genetic(data=data, param=param, i=i) +
+            ll.reporting(data=data, param=param, i=i)
+
 } # end ll.all
 

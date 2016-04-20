b7264566fdd5cc16da499694e416e76d6753d8e6
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-20 01:09:54 +0100

    now using closure for priors - same strategy as for likelihood

--- R/priors.R
+++ R/priors.R
@@ -1,36 +1,40 @@
-#' Priors of outbreaker2
-#'
-#' These functions compute various priors used in outbreaker.
-#'
-#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
-#'
-#' @rdname priors
-#'
-#' @export
-#'
-#' @param param a list containing parameters as returned by \code{outbreaker.mcmc.init}
-#' @param config a set of settings as returned by \code{\link{outbreaker.config}}
-#'
-#'
-prior.mu <- function(param, config){
-    return(stats::dexp(param$current.mu, 1000, log=TRUE))
+
+## In all of these functions:
+## --------------------------
+##
+## we return a function in which prior parameters are enclosed; see 'likelihood.R' for more detail
+## about the motivation (but basically, it's faster). These functions are later called by
+## 'create.priors()' which will attach prior values and make a list of functions with enclosed
+## parameters.
+
+## Arguments are:
+##
+## 'param' a list containing parameters as returned by \code{outbreaker.mcmc.init}
+## 'config' a list of named items containing input data as returned by \code{\link{outbreaker.config}}
+
+
+## We use an exponential prior for the mutation rate; the prior rate, which does not change in the
+## MCMC, is enclosed in the returned function.
+
+make.prior.mu <- function(param, config){
+    rate <- config$prior.mu
+    function(param) {
+        stats::dexp(param$current.mu, rate, log=TRUE)
+    }
 } # end prior.mu
 
 
 
-#' @rdname priors
-#' @export
-#'
-prior.pi <- function(param, config){
-    return(stats::dbeta(param$current.pi, 10, 1, log=TRUE))
+
+## We use a beta prior for the reporting probability (which contrains it to lie between 0 and 1);
+## the 2 shape parameters, which do not change in the MCMC, are enclosed in the returned function.
+
+make.prior.pi <- function(param, config){
+    shape1 <- config$prior.pi[1]
+    shape2 <- config$prior.pi[2]
+    function(param){
+        stats::dbeta(param$current.pi, shape1, shape2, log=TRUE)
+    }
 }
 
-
-
-#' @rdname priors
-#' @export
-#'
-prior.all <- function(param, config){
-    return(prior.mu(param=param) + prior.pi(param=param))
-} # end prior.all
 

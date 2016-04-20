59acb20cf5ce5de98ae9aab3ff8e9449183fd2be
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-20 02:53:31 +0100

    adding general posterior

--- R/create.posteriors.R
+++ R/create.posteriors.R
@@ -5,13 +5,16 @@
 
 create.posteriors <- function(loglike, priors){
 
-    ## These are all the functions generating various posterior functions;
-    ## we list them by alphabetic order
+    ## These are all the functions generating various posterior functions; we list them by
+    ## alphabetic order; note that unlike likelihood and prior functions, the '$all' here is not the
+    ## sum of the components of the list, but directly uses the 'all' functions in 'loglike' and
+    ## 'priors'.
+
 
     default.functions <- list(genetic = make.post.genetic,
-                              reporting = make.post.reporting
-                              )
-    post.function.names <- names(default.functions)
+                              reporting = make.post.reporting,
+                              all = make.post.all)
+
     out <- lapply(default.functions, function(f) f(loglike=loglike, priors=priors))
 
 }

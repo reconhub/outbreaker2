f53cc1464038cce96cb1f1df76714c1ceb78564f
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-20 02:33:31 +0100

    first version of create.posterior

--- R/create.posteriors.R
+++ R/create.posteriors.R
@@ -1,64 +1,28 @@
-#' Create posterior functions for outbreaker
-#'
-#' This function creates a named list of functions, each of which are compute log-posterior components of outbreaker.
-#' If these functions are provided as input, they will be used. Otherwise, default functions will be used.
-#'
-#' User-provided functions will be checked to ensure the right arguments are present. All log-posterior functions should have to following arguments:
-#' \describe{
-#' \item{data}{a list of named items containing input data as returned by \code{\link{outbreaker.data}}}
-#' \item{param}{a list of parameters as returned by \code{\link{outbreaker.mcmc.init}}} 
-#' }
-#'
-#' Note that unlike \code{outbreaker.create.moves}, posterior functions have no mandatory names. They only need to be compatible with the movement functions used in \code{outbreaker.create.moves}. See 'details' for defaults.
-#' 
-#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
-#'
-#' @param ... a named list (see details) of functions, each computing a log-posterior component.
-#' @param posteriors a list of functions as returned by \code{outbreaker.create.posteriors}
-#'
-#' @details
-#' The posterior functions used by default are:
-#' \describe{
-#' \item{genetic}{corresponds to the posterior distribution of the mutation rates; defaults to \code{\link{post.genetic}}}
-#' \item{all}{corresponds to the joint posterior; defaults to \code{\link{post.all}}}
-#' }
-#' 
-#' 
-#' @return a list of named functions
-#' 
-outbreaker.create.posteriors <- function(..., posteriors=NULL){
-    ## PROCESS ... ONLY IF NO MOVES IS PASSED
-    if(is.null(posteriors)){
-        posteriors <- list(...)
-    }
-    
-    ## SET DEFAULTS ##
-    defaults <- list(genetic = post.genetic,
-                     all = post.all
-                     )
 
-    ## MODIFY DEFAULTS WITH ARGUMENTS ##
-    posteriors <- modify.defaults(defaults, posteriors, strict=FALSE)
-    
+##
+## This function creates a named list of posterior functions with enclosed likelihood and prior functions.
+##
 
-    ## CHECK FUNCTIONS ##
-    check.function.args <- function(f){
-        args <- names(formals(f))
-        if(!identical(sort(args), c("data", "param"))) {
-            return(FALSE)
-        } else {
-            return(TRUE)
-        }
+create.posteriors <- function(loglike, priors){
+
+    ## These are all the functions generating various posterior functions;
+    ## we list them by alphabetic order
+
+    default.functions <- list(genetic = make.post.genetic,
+                              reporting = make.post.reporting
+                              )
+    post.function.names <- names(default.functions)
+    out <- lapply(default.functions, function(f) f(config))
+
+
+    ## We add a function summing all posteriors
+
+    out$all <- function(param){
+        sum(vapply(out[post.function.names], function(f) f(param), FUN.VALUE=numeric(1)), na.rm=TRUE)
     }
 
-    args.checks <- sapply(unlist(posteriors), check.function.args)
-    if(!all(args.checks)){
-        culprits <- names(args.checks)[!args.checks]
-        culprits <- paste(culprits, collapse=",")
-        stop("problems in movements of: ", culprits, "\narguments shoud be: 'data', 'param'")
-    }
-    
-    ## RETURN ##
-    return(posteriors)
-    
-} # end outbreaker.create.posteriors
+
+    return(out)
+
+
+}

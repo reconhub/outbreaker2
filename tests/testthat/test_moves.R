9a5de7d4d88482692331754a88edc610e12e9a6b
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-20 04:29:59 +0100

    fixed tests for movements

--- tests/testthat/test_moves.R
+++ tests/testthat/test_moves.R
@@ -12,6 +12,7 @@ test_that("parameters and augmented data move", {
     config <- outbreaker.config(data=data)
     config.no.move <- outbreaker.config(data=data, move.alpha=FALSE, move.t.inf=FALSE,
                                         move.mu=FALSE, move.pi=FALSE, move.kappa=FALSE)
+    data <- add.convolutions(data=data, config=config)
     param <- outbreaker.create.mcmc(data=data, config=config)
     ll <- create.loglike(data)
     priors <- create.priors(config)
@@ -22,13 +23,20 @@ test_that("parameters and augmented data move", {
     moves <- outbreaker.create.moves(config=config)
     moves.no.move <- outbreaker.create.moves(config=config.no.move)
 
-    ## test move.swap.cases ##
-    res <- moves$move.swap.cases(param=param, config=config, densities=densities, rand=rand)
-
-    expect_equal(length(param), length(res))
-    expect_equal(length(unlist(param)), length(unlist(res)))
-    expect_equal(names(param), names(res))
+    ## test moves lists ##
     expect_equal(length(moves.no.move), 0L)
+    expect_equal(length(moves), 6L)
+    expect_true(all(vapply(moves, is.function, logical(1))))
+
+    for(i in seq.int(length(moves))) {
+        res <- moves[[i]](param=param, config=config, densities=densities, rand=rand)
+
+        ## check that content in param after movements has identical shape
+        expect_equal(length(param), length(res))
+        expect_equal(length(unlist(param)), length(unlist(res)))
+        expect_equal(names(param), names(res))
+    }
+
 })
 
 

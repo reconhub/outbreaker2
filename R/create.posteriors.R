12861b1f903f38aecfc0ec2eb0a28ba736d050dc
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-20 02:39:20 +0100

    fixing passing of ars

--- R/create.posteriors.R
+++ R/create.posteriors.R
@@ -12,7 +12,7 @@ create.posteriors <- function(loglike, priors){
                               reporting = make.post.reporting
                               )
     post.function.names <- names(default.functions)
-    out <- lapply(default.functions, function(f) f(config))
+    out <- lapply(default.functions, function(f) f(loglike=loglike, priors=priors))
 
 
     ## We add a function summing all posteriors

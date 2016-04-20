69c9692b429ac3c14c572e883dc0d520b99af53d
thibautjombart <thibautjombart@gmail.com>
2016-04-20 17:21:20 +0100

    adding new function to create mcmc

---
+++ R/outbreaker.create.mcmc.R
@@ -0,0 +1,45 @@
+#' Initianilizes outputs for outbreaker
+#'
+#' This function creates initial outputs and parameter states for outbreaker.
+#'
+#' @author Thibaut Jombart (\email{t.jombart@@imperial.ac.uk})
+#'
+#' @param data a list of data items as returned by \code{outbreaker.data}
+#'
+#' @param config a list of settings as returned by \code{outbreaker.config}
+#'
+#' @export
+#'
+outbreaker.create.mcmc <- function(data, config){
+    ## CREATE EMPTY OUTPUT VECTORS ##
+    size <- round(config$n.iter/config$sample.every)
+    step <- integer(size)
+    post <- prior <- like <- mu <- pi <- double(size)
+    alpha <- as.list(integer(size))
+    t.inf <- as.list(integer(size))
+    kappa <- as.list(integer(size))
+
+    ## SET CURRENT VALUES AND COUNTER ##
+    step[1] <- 1L
+    current.mu <- mu[1] <- config$init.mu
+    current.alpha <- alpha[[1]] <- config$init.alpha
+    current.kappa <- kappa[[1]] <- config$init.kappa
+    current.pi <- pi[1] <- config$init.pi
+    if(is.null(config$init.t.inf)){
+        current.t.inf <- t.inf[[1]] <- data$dates - which.max(data$f.dens) + 1
+    } else {
+        current.t.inf <- config$init.t.inf
+    }
+    counter <- 1L
+
+
+    ## SHAPE CHAIN ##
+    out <- list(size=size, step=step,
+                post=post, like=like, prior=prior,
+                alpha=alpha, t.inf=t.inf, mu=mu, kappa=kappa, pi=pi,
+                current.alpha=current.alpha, current.t.inf=current.t.inf, current.mu=current.mu,
+                current.kappa=current.kappa, current.pi=current.pi,
+                counter=counter)
+    return(out)
+}
+

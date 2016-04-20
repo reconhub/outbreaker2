683d9dd3609032680b74d747ef7d650bea463582
Thibaut Jombart <thibautjombart@gmail.com>
2016-04-20 02:28:58 +0100

    now using the same strategy for posterior as for likelihood and priors

--- R/posteriors.R
+++ R/posteriors.R
@@ -1,37 +1,36 @@
-#' Posteriors of outbreaker2
-#'
-#' These functions compute various posteriors used in outbreaker.
-#'
-#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
-#'
-#' @rdname posteriors
-#'
-#' @param data a list of named items containing input data as returned by \code{\link{outbreaker.data}}
-#'
-#' @param param a list containing parameters as returned by \code{outbreaker.mcmc.init}
-#'
-post.genetic <- function(data, param){
-    return(ll.genetic(data=data, param=param) +
-           prior.mu(param=param))
-} # end post.genetic
+
+## In all of these functions:
+## --------------------------
+##
+## we return a function in which likelihood and prior functions are enclosed; see 'likelihood.R' for
+## more detail about the motivation (but basically, it's faster). These functions are later called
+## by 'create.posterior()' which will create a list of functions with enclosed
+## loglikelihood and prior functions.
+##
 
 
 
 
-#' @rdname posteriors
-#' @export
-#'
-post.reporting <- function(data, param){
-    return(ll.reporting(data=data, param=param) +
-           prior.pi(param=param))
-} # end post.reporting
+## The 'genetic posterior' includes the genetic likelihood and the priors for mutation rate 'mu'.
 
-
-
-
-#' @rdname posteriors
-#' @export
-#'
-post.all <- function(data, param){
-    return(ll.all(data=data, param=param) + prior.all(param))
+make.post.genetic <- function(loglike, priors){
+    ll.genetic <- loglike$genetic
+    prior.mu <- priors$mu
+    function(param){
+        ll.genetic(param) + prior.mu(param)
+    }
 }
+
+
+
+## The 'reporting posterior' includes the reporting likelihood and the priors for the reporting probability 'pi'.
+
+make.post.reporting <- function(loglike, priors){
+    ll.reporting <- loglike$reporting
+    prior.pi <- priors$pi
+    function(param){
+        ll.reporting(param) + prior.pi(param)
+    }
+}
+
+

bb90a8e8ab06b12b227ce3ca89f57c1a28ae237a
thibautjombart <thibautjombart@gmail.com>
2016-04-22 15:37:34 +0100

    renamed fakeOutbreak into fake.outbreak

--- R/data.R
+++ R/data.R
@@ -3,10 +3,10 @@
 #' This dataset is a small simulated outbreak used to illustrate outbreaker.
 #' It was generated using \code{simOutbreak}.
 #'
-#' @aliases fakeOutbreak
+#' @aliases fake.outbreak
 #' @docType data
 #' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
 #' @keywords datasets
-"fakeOutbreak"
+"fake.outbreak"
 
 

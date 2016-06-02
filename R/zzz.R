e38fb11043df8fb89c5968223eb52e9d6f082b6c
thibautjombart <thibautjombart@gmail.com>
2016-06-02 10:07:23 +0100

    commented out the startup msg (for now)

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-    pkg.version <- packageDescription("outbreaker2", fields = "Version")
-    startup.txt <- paste("\n   === outbreaker2", pkg.version, "is loaded ===\n")
-    startup.txt <- paste(startup.txt, "Information & Documentation -> check the R-epi project: \nhttp://sites.google.com/site/therepiproject\n", sep="\n")
-    startup.txt <- paste(startup.txt, "Questions -> ask the R-epi forum: \nr-epi@googlegroups.com", sep="\n")
-    packageStartupMessage(startup.txt)
+    ## pkg.version <- packageDescription("outbreaker2", fields = "Version")
+    ## startup.txt <- paste("\n   === outbreaker2", pkg.version, "is loaded ===\n")
+    ## startup.txt <- paste(startup.txt, "Information & Documentation -> check the R-epi project: \nhttp://sites.google.com/site/therepiproject\n", sep="\n")
+    ## startup.txt <- paste(startup.txt, "Questions -> ask the R-epi forum: \nr-epi@googlegroups.com", sep="\n")
+    ## packageStartupMessage(startup.txt)
 }

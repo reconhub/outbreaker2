3bf7b073edbf1e3da1569be126d1b3e48de33814
thibautjombart <thibautjombart@gmail.com>
2016-06-03 17:48:00 +0100

    making tests a tad more liberal to avoid random fails upon future changes

--- tests/testthat/test_outbreaker.R
+++ tests/testthat/test_outbreaker.R
@@ -81,8 +81,8 @@ test_that("test: convergence to decent results for toy example", {
 
     ## checks
     out.no.missing.smry <- summary(out.no.missing, burnin=1000)
-    expect_true(min(out.no.missing.smry$post) > -910) # approx log post values
-    expect_true(mean(out.no.missing.smry$tree$from==dat$ances, na.rm=TRUE) > .90) # at least 90% ancestries correct
+    expect_true(min(out.no.missing.smry$post) > -920) # approx log post values
+    expect_true(mean(out.no.missing.smry$tree$from==dat$ances, na.rm=TRUE) > .85) # at least 85% ancestries correct
     expect_true(mean(abs(out.no.missing.smry$tree$time - dat$onset), na.rm=TRUE)<3.5) # infection datewithin 3 days on average
 
 
